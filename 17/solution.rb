9e72188bd7585cb287c9f458ccb86ffa5bad0026
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2022-12-17 00:23:59 -0500

    Take notes

---
+++ 17/solution.rb
@@ -0,0 +1,32 @@
+# vertical chamber is 7 units wide
+# each rock appears two units away from the left
+#
+# five rock types:
+#
+# 1. ####
+#
+# 2. .#.
+#    ###
+#    .#.
+
+# 3. ..#
+#    ..#
+#    ###
+
+# 4. #
+#    #
+#    #
+#    #
+
+# 5. ##
+#    ##
+
+
+# tetris
+#
+
+# first jet stream push, then vertical down
+# each rock appears with bottom edge three units from floor or next rock
+# need to track the top
+
+
